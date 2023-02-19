81187687f6ccea66d1baeede72b78b015cfe18f3
satos310 <119788681+satos310@users.noreply.github.com>
2023-02-19 01:44:54 +0000

    [clear]4-25

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,11 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!, except: [:top]
   before_action :configure_permitted_parameters, if: :devise_controller?
+  
+  # ログアウト機能追加（自分で追加）
+  def after_sign_out_path_for(resource)
+    root_path 
+  end
 
   def after_sign_in_path_for(resource)
     post_images_path
